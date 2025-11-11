29ae9b0b82ed5f2dab034cd7b69fc15a149cdf26
artseth <artsethmedia01@gmail.com>
2025-11-11 06:07:21 -0500

    added control_flow.js and control_flow.html

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -52,3 +52,10 @@ switch(userType){
 }
 
 console.log("User Category:", userCategory);
+
+
+
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not Authenticated";
+
+console.log("Authentication Status:", authenticationStatus);
