277a0d97c734232e667067b0ab8bd8318117810c
artseth <artsethmedia01@gmail.com>
2025-11-11 18:55:15 +0200

    fixing a bug on the calculate_area.js file

--- calculateArea/calculate_area.js
+++ calculateArea/calculate_area.js
@@ -3,9 +3,9 @@ let width;
 
 function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
-    width = parseFloat(document.getElementById('width'));
+    width = parseFloat(document.getElementById('width').value);
 
-    let area = lenght * width;
+    let area = length * width;
 
     document.getElementById('result').innerText = `The area of the rectangle is:${area}`;
 }
